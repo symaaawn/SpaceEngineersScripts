ed85b1b8335c94959abfeced08c630e5c6702aeb
symaaawn <simon.neugebauer@gmail.com>
2025-09-19 20:24:33 +0200

    feat! update RelativePositioningSystemServiceMessage_Update

--- Ship.RelativePositioningSystemService.Contracts/RelativePositioningSystemServiceMessage_Update.cs
+++ Ship.RelativePositioningSystemService.Contracts/RelativePositioningSystemServiceMessage_Update.cs
@@ -24,16 +24,29 @@ namespace IngameScript
     {
         public class RelativePositioningSystemServiceMessage_Update : RelativePositioningSystemServiceMessage
         {
-            public Vector3D ReferenceCoordinates { get; internal set; }
+            public MatrixD ReferenceMatrix { get; internal set; }
 
             public override ImmutableDictionary<string, string> Serialize()
             {
                 return ImmutableDictionary<string, string>.Empty
                     .Add("RequestId", RequestId.ToString())
                     .Add("Method", Method)
-                    .Add("ReferenceCoordinates.X", ReferenceCoordinates.X.ToString())
-                    .Add("ReferenceCoordinates.Y", ReferenceCoordinates.Y.ToString())
-                    .Add("ReferenceCoordinates.Z", ReferenceCoordinates.Z.ToString());
+                    .Add("ReferenceCoordinates.M11", ReferenceMatrix.M11.ToString())
+                    .Add("ReferenceCoordinates.M12", ReferenceMatrix.M12.ToString())
+                    .Add("ReferenceCoordinates.M13", ReferenceMatrix.M13.ToString())
+                    .Add("ReferenceCoordinates.M14", ReferenceMatrix.M14.ToString())
+                    .Add("ReferenceCoordinates.M21", ReferenceMatrix.M21.ToString())
+                    .Add("ReferenceCoordinates.M22", ReferenceMatrix.M22.ToString())
+                    .Add("ReferenceCoordinates.M23", ReferenceMatrix.M23.ToString())
+                    .Add("ReferenceCoordinates.M24", ReferenceMatrix.M24.ToString())
+                    .Add("ReferenceCoordinates.M31", ReferenceMatrix.M31.ToString())
+                    .Add("ReferenceCoordinates.M32", ReferenceMatrix.M32.ToString())
+                    .Add("ReferenceCoordinates.M33", ReferenceMatrix.M33.ToString())
+                    .Add("ReferenceCoordinates.M34", ReferenceMatrix.M34.ToString())
+                    .Add("ReferenceCoordinates.M41", ReferenceMatrix.M41.ToString())
+                    .Add("ReferenceCoordinates.M42", ReferenceMatrix.M42.ToString())
+                    .Add("ReferenceCoordinates.M43", ReferenceMatrix.M43.ToString())
+                    .Add("ReferenceCoordinates.M44", ReferenceMatrix.M44.ToString());
             }
         }
     }
